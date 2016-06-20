02559f527b34e6494c8fdd757b4312d07ea4812c
maocw <952527136@qq.com>
2016-06-20 10:12:10 +0800

    bless

---
+++ test/src/main/java/mao/test/controller/TestController.java
@@ -0,0 +1,22 @@
+package mao.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+	@RequestMapping(value="/handlerAdapterTest",method=RequestMethod.GET)
+	@ResponseBody
+	public void handlerAdapter(){
+		//System.out.println("tests");
+	}
+	
+	@RequestMapping(value="/filterTest",method=RequestMethod.GET)
+	@ResponseBody
+	public void FilterTest(){
+		
+	}
+}
